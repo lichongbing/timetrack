2f3ca227830c8ecfc9a831f1a844a2e7b808f41d
lichongbing <873610008@qq.com>
2020-11-24 11:26:25 +0900

    Initial Commit

---
+++ timetrack/timetrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  timetrackApp.swift
+//  timetrack
+//
+//  Created by 李崇兵 on 2020/11/24.
+//
+
+import SwiftUI
+
+@main
+struct timetrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
